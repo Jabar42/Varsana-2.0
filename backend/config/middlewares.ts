59eae2be3ea1a8ac3fa21c9ebb8f56c6a60da46c
jaime barbudo <jaimebarbudomier@gmail.com>
2025-01-31 14:14:30 -0500

    Enhance security and CORS middleware configurations for local development

--- backend/config/middlewares.ts
+++ backend/config/middlewares.ts
@@ -1,8 +1,29 @@
 export default [
   'strapi::logger',
   'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        directives: {
+          'connect-src': ["'self'", 'http://localhost:3000'],
+          'img-src': ["'self'", 'data:', 'blob:', 'http://localhost:3000'],
+          'media-src': ["'self'", 'data:', 'blob:', 'http://localhost:3000'],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
+  {
+    name: 'strapi::cors',
+    config: {
+      enabled: true,
+      origin: ['http://localhost:3000'], // URL de tu frontend
+      methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+      headers: ['Content-Type', 'Authorization'],
+      credentials: true, // Habilita cookies y autenticación si es necesario
+    },
+  },
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',

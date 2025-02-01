e87a43274dc648c789da5129f0f7583753e1f34d
jaime barbudo <jaimebarbudomier@gmail.com>
2025-01-31 23:05:31 -0500

    Fix JSX configuration in tsconfig and adjust blog listing component for improved rendering

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -15,7 +15,7 @@ const App = () => {
         {blogs()?.data?.length === 0 && <p class="text-center text-gray-500">No hay blogs disponibles.</p>}
         
         {blogs()?.data?.map((blog) => (
-          <li key={blog.id} class="bg-white p-4 rounded-lg shadow-md hover:shadow-xl transition-shadow">
+          <li  class="bg-white p-4 rounded-lg shadow-md hover:shadow-xl transition-shadow">
             <h2 class="text-2xl font-semibold text-indigo-700">{blog.Title}</h2>
             <p class="mt-2 text-gray-700">
               {blog.Content?.map((contentItem, index) => (
